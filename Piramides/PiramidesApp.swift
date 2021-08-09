92a81a0896c4a136a22acf4b5e21dd200b21bdd6
Erik Leon <erikgleon@gmail.com>
2021-08-08 23:24:07 -0700

    Initial Commit

---
+++ Piramides/PiramidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiramidesApp.swift
+//  Piramides
+//
+//  Created by Erik Leon on 8/8/21.
+//
+
+import SwiftUI
+
+@main
+struct PiramidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
